fc0a7fd0047dedae0a199a416696eeca0398e32f
pseudocc <pseudoc@163.com>
2022-04-07 17:05:46 +0800

    fake an user agent

--- fetch.js
+++ fetch.js
@@ -19,7 +19,16 @@ export async function fetch_url(url, type, callback) {
   if (!domain)
     return;
   try {
-    const resp = await fetch(url);
+    const headers = {
+      'User-Agent': [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'AppleWebKit/537.36 (KHTML, like Gecko)',
+        'Chrome/100.0.4896.60',
+        'Safari/537.36',
+        'Edg/100.0.1185.29'
+      ].join(' ')
+    };
+    const resp = await fetch(url, { headers, method: 'GET' });
     if (!resp.ok)
       throw new Error(`Failed to fetch: ${resp.statusText}(${resp.status})`);
 
